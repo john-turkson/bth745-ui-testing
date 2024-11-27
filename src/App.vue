6ba286f4ed394b023512a0dc281c200eea443d74
john-turkson <you@example.com>
2024-11-27 15:08:49 -0500

    Initial Commit to repo

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <h1>Hello App!</h1>
+    <nav>
+      <RouterLink to="/">Go to Home</RouterLink>
+      <RouterLink to="/about">Go to About</RouterLink>
+    </nav>
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  methods: {
+    goToAbout() {
+      this.$router.push('/about')
+    },
+  },
+}
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold">
+    Hello world this the Home Page!
+  </h1>
+</template>
